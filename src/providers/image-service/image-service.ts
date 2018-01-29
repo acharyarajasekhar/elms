92abbb63df0ad137b6fafe0b9d567c8f78004d3e
rajavelur <rajvel.cse@gmail.com>
2018-01-29 12:00:29 +0530

    upload photo test 12
    upload photo test 12

--- src/providers/image-service/image-service.ts
+++ src/providers/image-service/image-service.ts
@@ -13,27 +13,22 @@ export class ImageProvider {
   file: File;
 
   private takePictureOptions: CameraOptions = {
-    quality: 50,
-    //allowEdit: false,
-    //saveToPhotoAlbum: true,
+    quality: 50,   
     targetWidth: 600,
-    targetHeight: 600,
-    //cameraDirection: this.camera.Direction.BACK,
-    //sourceType: this.camera.PictureSourceType.CAMERA,
+    targetHeight: 600,   
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
   }
 
   private galleryOptions: CameraOptions = {
-    allowEdit: true,
+    quality: 50,   
+    targetWidth: 600,
+    targetHeight: 600,   
     destinationType: this.camera.DestinationType.DATA_URL,
-    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
     encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    targetWidth: 720,
-    targetHeight: 720,
-    correctOrientation: true
+    mediaType: this.camera.MediaType.PICTURE,    
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY    
   }
 
   constructor( 
@@ -41,34 +36,30 @@ export class ImageProvider {
   }
 
   //Take a picture and return a promise with the image data
- async uploadFromCamera() {
-    /*this.camera.getPicture(this.takePictureOptions).then((imagePath) => {
-      alert('got image path ' + imagePath);
-      return this.makeFileIntoBlob(imagePath);//convert picture to blob
-    }).then((imageBlob) => {
-      alert('got image blob ' + imageBlob);
-      return this.uploadToFirebase(imageBlob);//upload the blob
-    }).then((uploadSnapshot: any) => {
-      alert('file uploaded successfully  ' + uploadSnapshot.downloadURL);
-      return this.saveToDatabase(uploadSnapshot);//store reference to storage in database
-    }).then((_uploadSnapshot: any) => {
-      alert('file saved to asset catalog successfully  ');
-    }, (_error) => {
-      alert('Error ' + (_error.message || _error));
-    });*/
+ async uploadFromCamera() {  
     try{
       const result = await this.camera.getPicture(this.takePictureOptions);
       const image = `data:image/jpeg:base64,${result}`;      
-      const pictures = storage().ref('pictures.jpg').put(this.dataURItoBlob(image));      
-      alert("photo uploaded successfully");
+      storage().ref().child('photos/' + localStorage.getItem('myId') + '.jpg').put(this.dataURItoBlob(image));
     }
     catch(e){
       console.error(e);
     }
   }
 
-  dataURItoBlob(dataURI) {
-    // code adapted from: http://stackoverflow.com/questions/33486352/cant-upload-image-to-aws-s3-from-ionic-camera
+   //open the gallery and Return a promise with the image data
+   async uploadFromGallery() {    
+      try{
+      const result = await this.camera.getPicture(this.galleryOptions);
+      const image = `data:image/jpeg:base64,${result}`;      
+      storage().ref().child('photos/' + localStorage.getItem('myId') + '.jpg').put(this.dataURItoBlob(image));
+      }
+      catch(e){
+        console.error(e);
+      }
+  }
+
+  dataURItoBlob(dataURI) {    
     let binary = atob(dataURI.split(',')[1]);
     let array = [];
     for (let i = 0; i < binary.length; i++) {
@@ -76,123 +67,4 @@ export class ImageProvider {
     }
     return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
   };
-
-  //open the gallery and Return a promise with the image data
-  uploadFromGallery() {
-    /*this.camera.getPicture(this.galleryOptions).then((imagePath) => {
-      alert('got image path ' + imagePath);
-      return this.makeFileIntoBlob(imagePath);//convert picture to blob
-    }).then((imageBlob) => {
-      alert('got image blob ' + imageBlob);
-      return this.uploadToFirebase(imageBlob);//upload the blob
-    }).then((uploadSnapshot: any) => {
-      alert('file uploaded successfully  ' + uploadSnapshot.downloadURL);
-      return this.saveToDatabase(uploadSnapshot);//store reference to storage in database
-    }).then((_uploadSnapshot: any) => {
-      alert('file saved to asset catalog successfully  ');
-    }, (_error) => {
-      alert('Error ' + (_error.message || _error));
-    });*/
-    this.camera.getPicture(this.galleryOptions).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64:
-      /*alert(imageData);
-      this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
-      alert(this.captureDataUrl);
-      let storageRef = firebase.storage().ref();
-      alert(storageRef);
-      // Create a timestamp as filename
-      const filename = Math.floor(Date.now() / 1000);
-      alert(filename);
-      // Create a reference to 'images/todays-date.jpg'
-      const imageRef = storageRef.child(`${filename}.jpg`);
-      alert(imageRef);  
-      imageRef.putString(this.captureDataUrl, 'data_url');*/
-      const image = `data:image/jpeg:base64,${imageData}`;          
-      const pictures = storage().ref('pictures');  
-      pictures.putString(image, 'data_url');
-      alert("photo uploaded");
-      //imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
-       //alert("photo uploaded" + snapshot.downloadURL);
-      //});
-    }, (err) => {
-      alert(err);
-    });
-  }
-
- /* makeFileIntoBlob(_imagePath) {
-    return new Promise((resolve, reject) => {
-      window.resolveLocalFileSystemURL(_imagePath, (fileEntry) => {
-
-        fileEntry.file((resFile) => {
-
-          var reader = new FileReader();
-          reader.onloadend = (evt: any) => {
-            var imgBlob: any = new Blob([evt.target.result], { type: 'image/jpeg' });
-            imgBlob.name = 'sample.jpg';
-            resolve(imgBlob);
-          };
-
-          reader.onerror = (e) => {
-            console.log('Failed file read: ' + e.toString());
-            reject(e);
-          };
-
-          reader.readAsArrayBuffer(resFile);
-        });
-      });
-    });
-  }
-
-  //Upload picture to the firebase storage
-  uploadToFirebase(imgBlob: any) {
-    var randomNumber = Math.floor(Math.random() * 256);
-    console.log('Random number : ' + randomNumber);
-    return new Promise((resolve, reject) => {
-      let storageRef = firebase.storage().ref(this.basePath + randomNumber + '.jpg');//Firebase storage main path
-
-      let metadata: firebase.storage.UploadMetadata = {
-        contentType: 'image/jpeg',
-      };
-
-      let uploadTask = storageRef.put(imgBlob, metadata);
-      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-        (snapshot) => {
-          // upload in progress
-          //let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          //console.log(progress);
-        },
-        (error) => {
-          // upload failed
-          console.log(error);
-          reject(error);
-        },
-        () => {
-          // upload success
-          let url = uploadTask.snapshot.downloadURL
-          console.log('Saved picture url', url);
-          resolve(uploadTask.snapshot);
-        });
-    });
-  }
-
-  saveToDatabase(uploadSnapshot) {
-    var ref = firebase.database().ref('photos');
-
-    return new Promise((resolve, reject) => {
-
-      // we will save meta data of image in database
-      var dataToSave = {
-        'URL': uploadSnapshot.downloadURL, // url to access file
-        'name': uploadSnapshot.metadata.name, // name of the file
-        'lastUpdated': new Date().getTime(),
-      };
-
-      ref.push(dataToSave, (response) => {
-        resolve(response);
-      });
-    });
-  }*/
-
-
 }
